62585afc9ecd59942a04f94c4239448ad67f7275
Dominic <dominicwong09@gmail.com>
2023-06-09 14:07:48 +1200

    updated checkouts controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,11 +1,17 @@
 class CheckoutsController < ApplicationController
     skip_before_action :is_authorized, :only => [:create, :complete]
     
+<<<<<<< HEAD
+=======
+    # Stripe.api_key = 'sk_test_51NGEqeCGX7XPMDHf8F2hkgKWMTldXx5PIUVuMm35dB30PQ1HnEWaKxVAZ0dttDRZKV0oxrK9CrIWu7atJe6Oa0yp00Vagwoyqp'
+
+>>>>>>> b7b95d4 (updated checkouts controller)
     def create
         dev_url = "http://localhost:8000"
         prod_url = ""
 
         session = Stripe::Checkout::Session.create({
+<<<<<<< HEAD
 			metadata: {
 				order_id: params['metadata']["order_id"]
 			},
@@ -53,6 +59,56 @@ class CheckoutsController < ApplicationController
 				},
 			],
             line_items: create_line_items(params),
+=======
+          shipping_address_collection: {allowed_countries: ['AU']},
+          shipping_options: [
+            {
+              shipping_rate_data: {
+                type: 'fixed_amount',
+                fixed_amount: {
+                  amount: 0,
+                  currency: 'aud',
+                },
+                display_name: 'Free shipping',
+                delivery_estimate: {
+                  minimum: {
+                    unit: 'business_day',
+                    value: 5,
+                  },
+                  maximum: {
+                    unit: 'business_day',
+                    value: 7,
+                  },
+                },
+              },
+            },
+            {
+              shipping_rate_data: {
+                type: 'fixed_amount',
+                fixed_amount: {
+                  amount: 1500,
+                  currency: 'aud',
+                },
+                display_name: 'Next day air',
+                delivery_estimate: {
+                  minimum: {
+                    unit: 'business_day',
+                    value: 1,
+                  },
+                  maximum: {
+                    unit: 'business_day',
+                    value: 1,
+                  },
+                },
+              },
+            },
+          ],
+            line_items: [{
+              # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+              price: 'price_1NGXOkHK0jF5AwAIDHPHUyTw',
+              quantity: 1,
+            }],
+>>>>>>> b7b95d4 (updated checkouts controller)
             mode: 'payment',
             success_url: dev_url + '/confirmation',
             cancel_url: dev_url + '?canceled=true',
