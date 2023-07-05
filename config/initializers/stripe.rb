807d6d00fc7945bb96757032b72372fac3f0f73a
Dominic <dominicwong09@gmail.com>
2023-07-05 14:06:17 +1200

    deploy

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
 # Stripe.api_key = Rails.application.credentials[:stripe][:secret]
-Stripe.api_key = Rails.application.credentials[:stripe][:test_secret]
+# Stripe.api_key = Rails.application.credentials[:stripe][:test_secret] #commented out for deploy
